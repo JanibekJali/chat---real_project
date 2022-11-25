52afb1a92fb4d9851b7ac24d9c7239b402cb699a
User <user@Users-MacBook-Pro.local>
2022-11-25 14:24:41 +0600

    Authentication is done

---
+++ lib/app/modules/authentication/authentication_service.dart
@@ -0,0 +1,153 @@
+import '../../data/models/user_model/user_model.dart';
+
+abstract class AuthenticationService {
+  Future<UserModel> getCurrentUser();
+  Future<UserModel> getCurrentCachedUser();
+  Future<dynamic> signInWithPhone(String phoneNumber);
+  // Future<UserModel> createWithEmailAndPassword(String email, String password);
+  // Future<dynamic> signInWithGoogle();
+  // Future<dynamic> signInWithFacebook();
+  Future<bool> signOut();
+}
+
+class FirebaseAuthenticationService extends AuthenticationService {
+  @override
+  Future<UserModel> getCurrentCachedUser() {
+    // TODO: implement getCurrentCachedUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<UserModel> getCurrentUser() {
+    // TODO: implement getCurrentUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future signInWithPhone(String phoneNumber) {
+    // TODO: implement signInWithPhone
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+}
+
+class AuthenticationException implements Exception {
+  final String message;
+
+  AuthenticationException({this.message = 'Unknown error occurred. '});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// abstract class KiruuServisi {
+//   Future<UserModel> getCurrentUser();
+//   Future<UserModel> signInWithPhone (String phoneNumber);
+
+// }
+// class FirebasedinKiruuServisi extends KiruuServisi{
+//   @override
+//   Future<UserModel> getCurrentUser() {
+//     // TODO(Janibek): implement getCurrentUser - tashtap ketiptir nege 
+//     // tashtady dep komandadan surasak bolot
+//     throw UnimplementedError();
+//   }
+
+//   @override
+//   Future<UserModel> signInWithPhone(String phoneNumber) {
+//     // TODO: implement signInWithPhone
+//     throw UnimplementedError();
+//   }
+
+// }
+
+// abstract class AuthenticationService {
+//   Future<UserModel> getCurrentUser();
+//   Future<UserModel> getCurrentCachedUser();
+//   Future<dynamic> signInWithEmailAndPassword(String email, String password);
+//   Future<UserModel> createWithEmailAndPassword(String email, String password);
+//   Future<dynamic> signInWithGoogle();
+//   Future<dynamic> signInWithFacebook();
+//   Future<bool> signOut();
+// }
+
+// class ServerAuthenticationService extends AuthenticationService {
+//   @override
+//   Future<UserModel> getCurrentUser() async {
+//     UserModel user = await firestoreAuthServices.currentUser();
+//     if (user != null) {
+//       return await userServices.getUser(user.id);
+//     } else {
+//       return null;
+//     }
+//   }
+
+//   @override
+//   Future<bool> signOut() async {
+//     try {
+//       return await firestoreAuthServices.signOut();
+//     } catch (e) {
+//       throw Exception(e);
+//     }
+//   }
+
+//   @override
+//   Future<UserModel> signInWithFacebook() async {
+//     return await firestoreAuthServices.signInWithFacebook();
+//   }
+
+//   @override
+//   Future<UserModel> signInWithGoogle() async {
+//     return await firestoreAuthServices.signInWithGoogle();
+//   }
+
+//   @override
+//   Future<UserModel> getCurrentCachedUser() {
+//     // TODO: implement getCurrentCachedUser
+//     throw UnimplementedError();
+//   }
+
+//   @override
+//   Future<dynamic> signInWithEmailAndPassword(
+//       String email, String password) async {
+//     return await firestoreAuthServices.signInWithEmailandPassword(
+//         email, password);
+//   }
+
+//   @override
+//   Future<UserModel> createWithEmailAndPassword(
+//       String email, String password) async {
+//     try {
+//       return await firestoreAuthServices.createUserWithEmailandPassword(
+//           email, password);
+//     } catch (e) {
+//       throw Exception(e);
+//     }
+//   }
+// }
+
+// class AuthenticationException implements Exception {
+//   final String message;
+
+//   AuthenticationException({this.message = 'Unknown error occurred. '});
+// }
---
+++ lib/app/modules/authentication/bindings/authentication_binding.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+class AuthenticationBiding extends Bindings {
+  @override
+  void dependencies() {
+    // Get.put<AuthenticationController>(
+    //     AuthenticationController(Get.put(ServerAuthenticationService())),
+    //     permanent: true);
+  }
+}
